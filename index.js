793f6e7d29b2c19f5cacd485ee5ab2c43c87bc26
N. Lucas <nal9@sfu.c>
2022-03-07 14:40:19 -0800

    forgot to uncomment line. admin/trade should work now

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 const {Pool} = require('pg');
 var pool = new Pool({
     connectionString: process.env.DATABASE_URL||"postgres://postgres:root@localhost/aio_dld_database"
-    //,ssl:{rejectUnauthorized: false}
+    ,ssl:{rejectUnauthorized: false}
 })
 
 express()
