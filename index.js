cafc3413b162d33b1fd750045d81e6cc97d039b9
Para <catiethehuman@gmail.com>
2022-03-20 18:11:34 -0700

    basic data routing for deck builder

--- index.js
+++ index.js
@@ -163,6 +163,35 @@ express()
       res.render('pages/pack-opener', data);
     })
 
+    //deck builder homepage
+    //list current users deck, along with a "New Deck" button
+    .get('/decks', async(req, res)=> {
+      try{
+        const client = await pool.connect();
+        var userDecksQuery = `SELECT * FROM decks WHERE owner_id=${req.session.user.id}`
+        const result = await client.query(userDecksQuery);
+        const data = {decks: result.rows};
+        res.render('pages/deckSelection', data);
+        client.release();
+      } catch(error) {
+        res.end(error);
+      }
+    })
+
+    //actual deck builder
+    .get('/build', async(req, res)=> {
+      const client = await pool.connect();
+      var userCollectionQuery = `SELECT cards FROM users WHERE id=${req.session.user.id}`
+      const collectionResult = await client.query(userCollectionQuery);
+      var cardsQuery = `SELECT * FROM cards`;
+      const cardsTable = await client.query(cardsQuery);
+      const data = {collection: collectionResult.rows[0].cards,
+                    cards: cardsTable.rows};
+      res.render('pages/deckBuilder', data);
+
+      client.release();
+    })
+
     // trading main page
     // send all user data to trade page
     .get('/trade', async(req, res)=>{
