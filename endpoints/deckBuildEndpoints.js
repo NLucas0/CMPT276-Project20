c6f038c0b911c9951976b1a7293bf1bff7020ee4
Para <catiethehuman@gmail.com>
2022-04-06 21:26:44 -0700

    deletion, deck selection page improved/cleaned up

--- endpoints/deckBuildEndpoints.js
+++ endpoints/deckBuildEndpoints.js
@@ -13,7 +13,10 @@ router.get('/decks', async (req, res) => {
         const client = await pool.connect();
         var userDecksQuery = `SELECT * FROM decks WHERE owner_id=${req.session.user.id}`
         const result = await client.query(userDecksQuery);
-        const data = {decks: result.rows};
+        var cardsQuery = `SELECT * FROM cards order by card_id`;
+        const cardsTable = await client.query(cardsQuery);
+        const data = {decks: result.rows,
+                      cards: cardsTable.rows};
         res.render('pages/deckSelection', data);
         client.release();
     } catch(error) {
@@ -34,8 +37,8 @@ router.get('/build', async(req, res) => {
         const cardsTable = await client.query(cardsQuery);
         var savedDeck = [];
         const data = {collection: collectionResult.rows[0].cards,
-                        cards: cardsTable.rows,
-                        deck: savedDeck};
+                      cards: cardsTable.rows,
+                      deck: savedDeck};
         res.render('pages/deckBuilder', data);
 
         client.release();
@@ -84,6 +87,26 @@ router.post('/save', async (req, res) => {
             await client.query(`UPDATE decks SET cards=$1, extra_deck=$2 WHERE owner_id=${req.session.user.id} AND name='${req.body.name}'`, [req.body.cards, req.body.extra]);
         }
         client.release();
+    } catch(error) {
+        res.redirect('/');
+    }
+})
+
+router.get('/delete/:id', async (req, res) => {
+    try {
+        if(!req.session.user) {
+            throw "Not Logged In";
+        }
+        const client = await pool.connect();
+        // var deckToDelete = await client.query(`SELECT * FROM decks where owner_id=${req.session.user.id} AND id='${req.params.id}'`);
+        // if(deckToDelete.rows > 0) {
+        //     console.log("DeckFound");
+            await client.query(`DELETE FROM decks WHERE owner_id=${req.session.user.id} AND id='${req.params.id}'`);
+        // } else {
+        //     throw "Deck belongs to different user or doesn't exist"
+        // }
+
+        res.redirect('/deckBuild/decks');
     } catch(error) {
         res.redirect('/');
     }
