016d474b3678078a31b5538743e3a878f6bbb439
Bruce Choi <yca388@sfu.ca>
2022-04-07 21:46:54 -0700

    even MOAR style improvements (login/signup page)

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,6 +1,7 @@
 // move focus to next text input or submit form
 function goToNext(id, event){
     if(event.key != 'Enter' && event.type != 'click'){
+        console.log("test");
         return;
     }
     if(id=="submit"){
