5ca91f2fca745f8af5589caaff11d85f298058e4
Bruce Choi <yca388@sfu.ca>
2022-04-07 23:53:30 -0700

    updated a test thing

--- test/cardViewerTest.js
+++ test/cardViewerTest.js
@@ -1,10 +1,14 @@
 describe('cardView', function() {
     it('should correctly load the data into the page', function(done) {
-        global.chai.request(global.server).get('/cardView')
-            .end(function(error,res) {
-                res.should.have.status(200);
-                done();
-            })
+        global.chai.request(global.server).post('/login').send({'username':'tester','password':'mctesty'})
+            .end(function(err, res) {
+            res.should.have.status(200);
+            global.chai.request(global.server).get('/cardView')
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                })
+            done();
+        });
     })
-
 })
